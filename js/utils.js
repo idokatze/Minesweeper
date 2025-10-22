f35e6e4de41318649edccf88d0ae5c330e2a406f
idokatze <idokatze@gmail.com>
2025-10-22 15:26:25 +0300

    Add files via upload
    Commit 1

---
+++ js/utils.js
@@ -0,0 +1,35 @@
+'use strict'
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min)
+    const maxFloored = Math.floor(max)
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled) // The maximum is exclusive and the minimum is inclusive
+}
+
+function makeId(length = 6) {
+    var txt = ''
+    var possible =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return txt
+}
+
+function getRandomColor() {
+    const excludedColor = 'rgb(0, 0, 128)'
+
+    while (true) {
+        const r = Math.floor(Math.random() * 256) // Random red value (0-255)
+        const g = Math.floor(Math.random() * 256) // Random green value (0-255)
+        const b = Math.floor(Math.random() * 256) // Random blue value (0-255)
+        var color = `rgb(${r}, ${g}, ${b})`
+        if (color !== excludedColor) return color
+    }
+}
